28374d71911a70e1a1a1323ca504510b723b9ac5
Mudassar Hanif <mudassarhanif2@gmail.com>
2020-06-08 01:03:38 -1100

    Created Basic React App

---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Hello.css'
+
+function Hello({firstName}) {
+  return <p className="myname">This is para in Hello tag name : {firstName}</p>
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name="Zia Khan" age={25} isReactDevelper = {true}/></div>,
   document.getElementById('root')
 );
+
+/*
+ReactDOM.render(
+  React.createElement('div', null, React.createElement('span', null, 'Hello in span created element')),
+  document.getElementById('root')
+);
+*/
+/*
+ReactDOM.render(
+  <div><App/></div>,>>> The target can be acheived using createElement()
+  document.getElementById('root')
+);*/
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
